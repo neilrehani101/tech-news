578e52c685c9ce5cc91a3173a5687024c57c1792
TechBoy Corp <78197140+techboycorp1409@users.noreply.github.com>
2021-05-14 15:23:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,62 @@
-console.log("This is my index js file");
+document.getElementById('search-btn').addEventListener('click', function (e) {
+    console.log("This is my index js file");
+    let newsTopic = document.getElementById('newsTopic');
+    let topic = newsTopic.value;
+    let topicFine = topic.replaceAll(" ", "%20")
+    // Initialize the news api parameters
+    let apiKey = 'P7q5qZnuXZ52BOa1C2AW8gulvrsN9qz6'
 
-// Initialize the news api parameters
-let apiKey = 'P7q5qZnuXZ52BOa1C2AW8gulvrsN9qz6'
+    // Grab the news container
+    let newsAccordion = document.getElementById('newsAccordion');
 
-// Grab the news container
-let newsAccordion = document.getElementById('newsAccordion');
+    // Create an ajax get request
+    const data = null;
 
-// Create an ajax get request
-const xhr = new XMLHttpRequest();
-xhr.open('GET', `https://api.nytimes.com/svc/topstories/v2/world.json?api-key=P7q5qZnuXZ52BOa1C2AW8gulvrsN9qz6`, true);
+    const xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
 
-// What to do when response is ready
-xhr.onload = function () {
-    if (this.status === 200) {
-        let json = JSON.parse(this.responseText);
-        let articles = json.results;
-        console.log(articles);
-        let newsHTML = "";
-        articles.forEach((element, index) => {
-            // console.log(articles[news])
-            let newsAc = `<div class="accordion-item">
+    // xhr.addEventListener("readystatechange", function () {
+    //     if (this.readyState === this.DONE) {
+    //         console.log(this.responseText);
+    //     }
+    // });
+
+    xhr.open("GET", `https://free-news.p.rapidapi.com/v1/search?q=${topicFine}&lang=en`, true);
+    xhr.setRequestHeader("x-rapidapi-key", "23ff708fa5msh379bac8248fa7f3p1a13c3jsnfa084f189834");
+    xhr.setRequestHeader("x-rapidapi-host", "free-news.p.rapidapi.com");
+    // What to do when response is ready
+    xhr.onload = function () {
+        if (this.status === 200) {
+            let json = JSON.parse(this.responseText);
+            console.log(json);
+            if (json.status === "No matches for your search.") {
+                newsAccordion.innerHTML = "<h3>We have no matches for your search. Maybe you typed the topic wrong? Please check.<h3>";
+            } else if (json.status = "ok") {
+                document.getElementById('head').innerHTML = `News for topic: "${topic}"`
+                let articles = json.articles;
+                console.log(articles);
+                let newsHTML = "";
+                articles.forEach((element, index) => {
+                    // console.log(articles[news])
+                    let newsAc = `<div class="accordion-item">
                 <h2 class="accordion-header" id="flush-heading${index}">
                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse${index}" aria-expanded="false" aria-controls="flush-collapse${index}">
                         ${element["title"]}
                     </button>
                 </h2>
                 <div id="flush-collapse${index}" class="accordion-collapse collapse" aria-labelledby="flush-heading${index}" data-bs-parent="#newsAccordion">
-                    <div class="accordion-body"> ${element["abstract"]} <div><small>Read more <a href="${element["url"]}" target="_blank" >here</a></small></div></div>
+                    <div class="accordion-body"> ${element["summary"]}... <div><small>Read more <a href="${element["link"]}" target="_blank">here</a></small></div></div>
                 </div>
             </div>`
-            newsHTML += newsAc;
-        });
-        newsAccordion.innerHTML = newsHTML;
-    } else {
-        console.log("Some error occured")
+                    newsHTML += newsAc;
+                });
+                newsAccordion.innerHTML = newsHTML;
+            } else {
+                console.log("Some error occured")
+            }
+        }
+        //"<h1>We have no matches for your search. Maybe you types the keyword wrong? Please check.<h1>"
     }
-}
 
-xhr.send()
-
+    xhr.send(data);
+})
